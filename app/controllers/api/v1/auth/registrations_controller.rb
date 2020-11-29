65007e3332fd04c8658be3de3c70452ce96537a5
Kenchiki-chiki <kodama303@gmail.com>
2020-11-29 19:16:40 +0900

    registrationsコントローラーの削除

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,2 @@
-class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-end
+# class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
-    }
+      mount_devise_token_auth_for 'User', at: 'auth'
+    #   , controllers: {
+    #     registrations: 'api/v1/auth/registrations'
+    # }
     end
   end
   
