66dc7543e89e510e79bd6323cee31665e91e838d
Kenchiki-chiki <kodama303@gmail.com>
2020-12-15 21:46:49 +0900

    スキル設定機能の追加

--- app/controllers/api/v1/skills_controller.rb
+++ app/controllers/api/v1/skills_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::SkillsController < ApplicationController
-  before_action :authenticate_api_v1_user!
+  
 
   def index
     skills = Skill.all
@@ -8,8 +8,6 @@ class Api::V1::SkillsController < ApplicationController
 
   def create
     Skill.transaction do
-      current_api_v1_user = User.find_by(email: request.headers[:Uid])
-      binding.pry
       skill_params[:_json].each do |name|
         current_api_v1_user.skills.create!(name: name)
       end
