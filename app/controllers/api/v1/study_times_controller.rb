bde9ca1033f693f097d1bcfb50dc64a18d47dc1e
Matsushin <matsushin.dev@gmail.com>
2020-12-29 21:29:53 +0900

    学習時間を登録できるように

--- app/controllers/api/v1/study_times_controller.rb
+++ app/controllers/api/v1/study_times_controller.rb
@@ -4,7 +4,14 @@ class Api::V1::StudyTimesController < ApplicationController
   end
 
   def create
-    binding.pry
+    StudyTime.transaction do
+      params[:study_times].each do |param|
+        next if param[:study_hour].blank?
+        skill = current_user.skills.find(param[:skill_id])
+        skill.study_times.create!(study_hour: param[:study_hour], studied_on: Date.current)
+      end
+    end
+    #binding.pry
     # studyTime = StudyTime.transaction do
     #   study_time_params[:_json].each do |study_hour|
     #     current_api_v1_user.skills.study_hour.create!(study_hour: study_hour)
@@ -21,7 +28,7 @@ class Api::V1::StudyTimesController < ApplicationController
     # {"_json"=>["1", "0.5"], "controller"=>"api/v1/study_times", "action"=>"create", "study_time"=>{}} permitted: false>
     # {"0"=>"1.5", "1"=>"0.5", "controller"=>"api/v1/study_times", "action"=>"create", "study_time"=>{}} permitted: false>
 
-    # Parameters: {{skill_id: 1, study_hour: 4, studied_on: 2020.12.26}, {skill_id: 2, study_hour: 6, studied_on: 2020.12.26}}
-    params.permit(_json:[])
+    # Parameters: { study_time => [{skill_id: 1, study_hour: 4}, {skill_id: 2, study_hour: 6}] }
+    #params.permit(study_time: [])
   end
 end
