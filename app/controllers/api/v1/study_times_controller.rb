33f056d575c8e1db87ed3b7ad97956c2a7941f67
Matsushin <matsushin.dev@gmail.com>
2021-01-03 19:37:53 +0900

    合計24時間のチェックが効くように

--- app/controllers/api/v1/study_times_controller.rb
+++ app/controllers/api/v1/study_times_controller.rb
@@ -8,8 +8,8 @@ class Api::V1::StudyTimesController < ApplicationController
   
 
   def create
-
-    studyTime = StudyTime.transaction do
+    studyTime = 0
+    StudyTime.transaction do
       params[:study_times].each do |param|
         next if param[:study_hour].blank?
         existingStudyTime = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: Date.current} ).sum(:study_hour)
@@ -18,24 +18,24 @@ class Api::V1::StudyTimesController < ApplicationController
         # binding.pry
         studyTime = existingStudyTime + paramStudyTime
         # binding.pry
-        
-        if studyTime <= 24         
-            skill =current_api_v1_user.skills.find(param[:skill_id])
-            skill.study_times.create!(study_hour: param[:study_hour], studied_on: Date.current)
-        else
-          raise "1日の学習時間が24時間を超えています。"
-          render json: { error: studyTime.errors.full_messages }
-        end
 
+        if studyTime <= 24
+          skill = current_api_v1_user.skills.find(param[:skill_id])
+          skill.study_times.create(study_hour: param[:study_hour], studied_on: Date.current)
+        else
+          raise ActiveRecord::Rollback
+        end
       end
     end
-    
-    # totalStudyTimes = Skill.joins(:study_times).where(study_times: { studied_on: Date.current}, user_id: current_api_v1_user.id ).pluck(:study_hour)
-    totalStudyTimes = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: Date.current} ).pluck(:study_hour)
 
-    render json: totalStudyTimes, status: :created
+    if studyTime > 24
+      render json: { errors: ["1日の学習時間が24時間を超えています。"] }, status: :created
+    else
+      # totalStudyTimes = Skill.joins(:study_times).where(study_times: { studied_on: Date.current}, user_id: current_api_v1_user.id ).pluck(:study_hour)
+      totalStudyTimes = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: Date.current} ).pluck(:study_hour)
 
-
+      render json: totalStudyTimes, status: :created
+    end
   end
 
   # existingStudyTime = StudyTime.where(studied_on: Date.current).sum(:study_hour)
--- app/models/study_time.rb
+++ app/models/study_time.rb
@@ -1,4 +1,14 @@
 class StudyTime < ApplicationRecord
+  MAX_DAY_HOUR = 24
   validates :study_hour, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 24 }, allow_blank: true
   belongs_to :skill
+
+  validate :less_than_day_hour
+
+  def less_than_day_hour
+    study_hours = skill.user.study_times.where(studied_on: Date.current).sum(:study_hour)
+    if (study_hours.to_i + study_hour) > MAX_DAY_HOUR
+      errors.add(:study_hour, '合計24時間を超えています。')
+    end
+  end
 end
