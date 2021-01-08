1705e35258a0113f3a7bd897b3c146f3b9b251d8
Kenchiki-chiki <kodama303@gmail.com>
2021-01-08 20:11:32 +0900

    スキル別の学習時間をフロント側に返す記述を修正

--- app/controllers/api/v1/pie_charts_controller.rb
+++ app/controllers/api/v1/pie_charts_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::PieChartsController < ApplicationController
   def index
-    byCategoryStudyTimes = []
-
-    # byCategoryStudyTimes << current_api_v1_user.skills.joins(:study_times).group(:skill).sum(:study_hour)
-    byCategoryStudyTimes << current_api_v1_user.skills.joins(:study_times).select('owners.*, study_times.*').group(:skill).sum(:study_hour)
-    binding.pry
-    render json: byCategoryStudyTimes, status: :created
+    bySkillStudyTimes = []
+    bySkillStudyTimes << current_api_v1_user.skills.joins(:study_times).group(:name).sum(:study_hour)
+  
+    render json: bySkillStudyTimes, status: :created
 
   end
 end
