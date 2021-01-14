5c806839dcb266520f32c46d95d6637f3c5d9de7
Kenchiki-chiki <kodama303@gmail.com>
2021-01-15 01:10:26 +0900

    クリックした日付の学習時間をフロント側に返せるように修正

--- app/controllers/api/v1/calendars_controller.rb
+++ app/controllers/api/v1/calendars_controller.rb
@@ -1,7 +1,28 @@
 class Api::V1::CalendarsController < ApplicationController
   def index
+    
     # totalStudyTimes = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: Date.current} ).pluck(:study_hour)
 
-    # render json: totalStudyTimes, status: :created
+    # パターン１
+    # tahtDayTotalStudyTimes = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: params[:date].to_date} ).pluck(:study_hour)
+
+    # パターン２
+    clickedDate = params[:date].to_date
+    tahtDayTotalStudyTimes = current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: clickedDate} ).pluck(:study_hour)
+
+    # binding.pry
+
+    render json: tahtDayTotalStudyTimes, status: :created
   end
 end
+
+
+# パラメーター
+# { calendar => { 2021/1/12 } }
+
+# URLに日付を組み込む
+# http://localhost:3000/that_day_study_time/20210119
+# http://localhost:3000/that_day_study_time/2021/01/19
+# http://localhost:3000/that_day_study_time/2021/1/19
+
+# カレンダークリック→_date.vueに遷移
