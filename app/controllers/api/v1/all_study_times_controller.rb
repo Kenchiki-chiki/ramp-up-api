f78037c3c8b6e94a592bfcee83498138c28a93f3
Kenchiki-chiki <kodama303@gmail.com>
2021-01-23 00:37:06 +0900

    変数名の修正

--- app/controllers/api/v1/all_study_times_controller.rb
+++ app/controllers/api/v1/all_study_times_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AllStudyTimesController < ApplicationController
   LONG_STUDY_HOURS = 8
   MIDDLE_STUDY_HOURS = 4
-  SHORT_STUDY_HOURS = 0
+  ZERO_STUDY_HOURS = 0
   def index
     all_study_times_hashs = current_api_v1_user.study_times.select('SUM(study_hour) as total_study_hour, studied_on').group(:studied_on).map{|s| {total_study_hour: s.total_study_hour, studied_on: s.studied_on}}
     
@@ -10,7 +10,7 @@ class Api::V1::AllStudyTimesController < ApplicationController
         hash["color"] = "rgba(102, 200, 232, 0.9)"
       elsif hash[:total_study_hour] >= MIDDLE_STUDY_HOURS
         hash["color"] = "rgba(255, 196, 12, 0.9)"
-      elsif hash[:total_study_hour] > SHORT_STUDY_HOURS
+      elsif hash[:total_study_hour] > ZERO_STUDY_HOURS
         hash["color"] = "rgba(248, 121, 121, 0.9)"
       else
 
