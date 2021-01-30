f922b08b660088582a56a390c6dac7c263cd47f1
Kenchiki-chiki <kodama303@gmail.com>
2021-01-30 13:13:10 +0900

    未ログインユーザーの処理制限機能を追加

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TasksController < ApplicationController
-
+  before_action :authenticate_api_v1_user!
   def index
     tasks = current_api_v1_user.tasks.order(:id)
     render json: tasks
