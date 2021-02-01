56d98b893e2ceb872c91a8b5ff00c501f738c95c
Kenchiki-chiki <kodama303@gmail.com>
2021-02-01 22:18:09 +0900

    今月以外の学習時間が棒グラフに反映されるように修正

--- app/controllers/api/v1/bar_charts_controller.rb
+++ app/controllers/api/v1/bar_charts_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::BarChartsController < ApplicationController
   def index
     monthly_study_times = []
     now = Time.current
-    (1..11).each do |i|
-      monthly_study_times << current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: now.ago(i.month)} ).sum(:study_hour)
+    (1..11).reverse_each do |i|
+      monthly_study_times << current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: now.ago(i.month).all_month} ).sum(:study_hour)
     end
     monthly_study_times << current_api_v1_user.skills.joins(:study_times).where(study_times: { studied_on: Time.now.all_month} ).sum(:study_hour)
     render json: monthly_study_times, status: :created
